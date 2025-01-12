9335ebf1d90b32ccbc29fff9dfd20f29b4822ce5
atascg01 <atascg01@estudiantes.unileon.es>
2025-01-12 20:12:02 +0100

    Added Becook pic

--- src/data/restaurantsData.ts
+++ src/data/restaurantsData.ts
@@ -1,8 +1,10 @@
 import restaurant from '../images/restaurants/restaurant.jpg';
+import becook from '../images/restaurants/becook.jpeg';
+
 import { FoodType, Restaurant } from '../interfaces/Restaurant';
 
 const restaurantsData : Restaurant[] = [
-  { name: 'Becook', img: restaurant, foodType: FoodType.SignatureCuisine, url: 'becook'},
+  { name: 'Becook', img: becook, foodType: FoodType.SignatureCuisine, url: 'becook'},
   { name: 'Clandestino', img: restaurant, foodType: FoodType.SignatureCuisine, url: 'clandestino' },
   { name: 'Kiosko de Pizza', img: restaurant, foodType: FoodType.Pizza, url: 'kiosko-de-pizza' },
   { name: 'La Consentida', img: restaurant, foodType: FoodType.Brunch, url: 'la-consentida' },
